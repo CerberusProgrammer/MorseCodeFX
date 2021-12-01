9b8dbac8ee890ffe829e174ca2295348f1ab3b4e
Cerberus <cerberusprogrammer@gmail.com>
2021-12-01 12:06:28 -0800

    new

---
+++ src/BinaryTree/ControllerBinaryTree.java
@@ -0,0 +1,19 @@
+package BinaryTree;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerBinaryTree implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/Start.java
+++ src/Start.java
@@ -3,13 +3,15 @@ import BinaryTree.BinaryTree;
 public class Start {
 
     public static void main(String[] args) {
-        BinaryTree<String> binaryTree = new BinaryTree<String>();
-        binaryTree.insertar("a");
-        binaryTree.insertarHijoDerecho(binaryTree.getRaiz(), "s");
-        binaryTree.insertar("x");
-        binaryTree.insertar("b");
-        binaryTree.insertar("c");
-        binaryTree.insertar("d");
+        BinaryTree<Integer> binaryTree = new BinaryTree<Integer>();
+        binaryTree.insertar(1);
+        System.out.println("Raiz: " + binaryTree.getRaiz());
+        binaryTree.insertar(2);
+        binaryTree.insertar(0);
+        binaryTree.insertar(4);
+        binaryTree.insertar(9);
+        binaryTree.insertar(10);
+        binaryTree.insertar(5);
 
         System.out.println(binaryTree.recorridoInOrden().toString());
     }
